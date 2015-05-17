811cf38b9ba93c6d3e728396527fddfe462bbb6f
Wu Chih-En <trunone@gmail.com>
2015-05-17 14:44:36 +0800

    add team state machine

--- strategy/src/team_strategy.cpp
+++ strategy/src/team_strategy.cpp
@@ -3,58 +3,20 @@
 #include <actionlib/client/terminal_state.h>
 #include <robot_task/RobotTask.h>
 #include <robot_task/RobotTaskAction.h>
+
+#include "fsm/fsm_team_strategy.h"
+
 using namespace std;
 using namespace robot_task;
-
-typedef actionlib::SimpleActionClient<RobotTaskAction> Client;
 
 int main (int argc, char **argv)
 {
     ros::init(argc, argv, "team_strategy");
     ros::NodeHandle nh;
     //    = nh_.subscribe("/random_number", 1, );
+    ros_decision_making_init(argc, argv);
 
-    // create the action client
-    actionlib::SimpleActionClient<RobotTaskAction> ac("roles");
-
-    ROS_INFO("Waiting for action server to start.");
-
-	bool serverFound = false;
-	while( !serverFound && ros::ok() ){
-		ac.waitForServer(ros::Duration(1.0));
-		if(ac.isServerConnected()){
-			ROS_INFO("... Task is found.");
-			serverFound = true;
-		}else{
-			ROS_INFO("... Task not found, retry...");
-		}
-	}
-
-    while(ros::ok()) {
-
-	// send a goal to the action
-	RobotTaskGoal goal;
-		goal.name = "goalie";
-		stringstream suid; suid<<"task_id_tested_"<<::time(NULL);
-		goal.uid = suid.str();
-		goal.parameters="";
-		//if(find(params,"arg=").size()!=0)
-		//	goal.parameters = value(params, "arg=");
-		//else if(find(params,"args=").size()!=0)
-		//	goal.parameters = value(params, "args=");
-        ac.sendGoal(goal);
-
-        //wait for the action to return
-        bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
-
-        if (finished_before_timeout)
-        {
-            actionlib::SimpleClientGoalState state = ac.getState();
-            ROS_INFO("Action finished: %s",state.toString().c_str());
-        }
-        else
-            ROS_INFO("Action did not finish before the time out.");
-    }
+    LocalTasks::registrate("goalie", callTask);
 
     //exit
     return 0;
