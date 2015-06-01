53fe81fecd39a7e60e9be06192c8bdffc480e92b
Wu Chih-En <trunone@gmail.com>
2015-06-02 05:31:28 +0800

    test move_in_line done

--- path_planning/scripts/move_in_line.py
+++ path_planning/scripts/move_in_line.py
@@ -10,9 +10,10 @@ import numpy
 
 class MoveInLine:
     def __init__(self, name):
-        self.pub = None
-        self.sub = None
         self.target = None
+
+        self.sub = rospy.Subscriber("/robot_pose", Pose2D, self.go_to_cb)
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
         self._as = SimpleActionServer(name, GoToPoseAction, auto_start=False)
         self._as.register_goal_callback(self.goal_cb)
@@ -22,26 +23,24 @@ class MoveInLine:
         rospy.loginfo("Creating ActionServer [%s]", name)
 
     def goal_cb(self):
-        self.sub = rospy.Subscriber("/world_model/robot_pose", Pose2D, self.go_to_cb)
-        self.pub = rospy.Publisher("/cmd_vel", Twist)
 
-        self.target = accept_new_goal().pose2d
+        self.target = self._as.accept_new_goal().pose2d
 
-    def preempt_cb():
-        self.pub.unregister()
-        self.sub.unregister()
+    def preempt_cb(self):
         self._as.set_preempted()
 
     def go_to_cb(self, msg):
-        if target == msg:
-            self.pub.unregister()
-            self.sub.unregister()
-            self._as.set_succeeded()
+        if not self._as.is_active():
+            return
+
         np_target = numpy.array([self.target.x, self.target.y])
         np_robot = numpy.array([msg.x, msg.y])
-        np_vel = np_target - np_robot
-        ang_vel = msg.theta - target.theta
 
+        if (np_target == np_robot).all() and (msg.theta == self.target.theta):
+            self._as.set_succeeded()
+
+        np_vel = np_target - np_robot
+        ang_vel = abs(msg.theta - self.target.theta)
         self.pub.publish(Twist(Vector3(np_vel[0], np_vel[1], 0), Vector3(0, 0, ang_vel)))
 
 def main():
---
+++ path_planning/test/test_move_in_line.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+
+from actionlib import *
+from geometry_msgs.msg import *
+from fira_msl_msgs.msg import *
+
+def test_client():
+
+    client = SimpleActionClient('move_in_line', GoToPoseAction)
+
+    client.wait_for_server()
+
+    goal = GoToPoseGoal(Pose2D(3, -2, 2))
+
+    client.send_goal(goal)
+
+    #client.wait_for_result()
+
+    return
+
+if __name__=='__main__':
+    rospy.init_node('test_move_in_line')
+    test_client()
+
