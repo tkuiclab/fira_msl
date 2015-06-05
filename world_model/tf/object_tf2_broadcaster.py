3a3648b8c927d01f93d7ebed7285113b01abaf8c
Wu Chih-En <trunone@gmail.com>
2015-06-06 02:11:12 +0800

    add goal tf broadcaster

--- world_model/tf/object_tf2_broadcaster.py
+++ world_model/tf/object_tf2_broadcaster.py
@@ -7,7 +7,7 @@ import tf2_ros
 from std_msgs.msg import *
 from geometry_msgs.msg import *
 
-def handle_robot_pose(msg, name):
+def handle_object_pose(msg, name):
     br = tf2_ros.TransformBroadcaster()
     [x, y, z, w] = tf.transformations.quaternion_from_euler(0, 0, msg.theta)
     br.sendTransform(TransformStamped(
@@ -16,10 +16,10 @@ def handle_robot_pose(msg, name):
             Transform(Vector3(msg.x, msg.y, 0), Quaternion(x, y, z, w))))
 
 if __name__ == '__main__':
-    rospy.init_node('robot_tf_broadcaster')
-    robot_name = rospy.get_param('~robot_name')
-    rospy.Subscriber('/%s/pose'%robot_name,
+    rospy.init_node('object_tf_broadcaster')
+    object_name = rospy.get_param('~object_name')
+    rospy.Subscriber('/%s/pose'%object_name,
                      Pose2D,
-                     handle_robot_pose,
-                     robot_name)
+                     handle_object_pose,
+                     object_name)
     rospy.spin()
