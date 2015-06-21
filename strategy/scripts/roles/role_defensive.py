a2b8a4a34b8a6e1a908f214c3d5ba2c22eb00bbf
Wu Chih-En <trunone@gmail.com>
2015-06-22 01:44:27 +0800

    add role defensive

---
+++ strategy/scripts/roles/role_defensive.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import rospy
+
+import actionlib
+
+from actionlib.msg import *
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import *
+
+from std_srvs.srv import *
+
+# State machine classes
+import smach
+from smach import *
+from smach_ros import *
+
+
+def main():
+    rospy.init_node("role_defensive")
+
+    # Construct state machine
+    offensive_sm = StateMachine(
+            outcomes=['succeeded','aborted','preempted'])
+
+    # Set the initial state explicitly
+    offensive_sm.set_initial_state(['SEARCH_BALL'])
+
+    with offensive_sm:
+        StateMachine.add('SEARCH_BALL',
+                SimpleActionState('bhv_search_ball', TestAction),
+                { 'succeeded':'CHASE_BALL' })
+
+        StateMachine.add('CHASE_BALL',
+                SimpleActionState('bhv_chase_ball', TestAction),
+                { 'succeeded':'DASH', 'aborted': 'SEARCH_BALL' })
+
+        StateMachine.add('DASH',
+                SimpleActionState('bhv_dash', TestAction),
+                { 'succeeded':'succeeded', 'aborted': 'SEARCH_BALL' })
+
+
+    # Run state machine introspection server
+    intro_server = IntrospectionServer('defensive',offensive_sm,'/ROLE_SELECTOR/DEFENSIVE')
+    intro_server.start()
+
+    # Run state machine action server
+    sms = ActionServerWrapper(
+            'role_defensive', TestAction, offensive_sm,
+            succeeded_outcomes = ['succeeded'],
+            aborted_outcomes = ['aborted'],
+            preempted_outcomes = ['preempted'],
+            goal_slots_map = {},
+            result_slots_map = {})
+    sms.run_server()
+
+    rospy.spin()
+
+    intro_server.stop()
+
+if __name__ == "__main__":
+    main()
